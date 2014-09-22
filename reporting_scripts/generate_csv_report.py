901a4975bee0a431d9dc5acb6eaab64559fd105e
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-09-22 16:10:33 -0400

    Update generate_csv_report.py

--- reporting_scripts/generate_csv_report.py
+++ reporting_scripts/generate_csv_report.py
@@ -4,6 +4,18 @@ If the data is very big, this module will split the data into a number of csv fi
 rows, and user wants csv file to have only 5000 rows, then this module would split it into 3 csv files with 5000, 5000
 and 200 rows
 
+The CSV constructor has 2 required parameters: csv_data and list_of_headers and 2 optional parameters: output_file
+and row_limit. 
+
+Usage:
+
+from generate_csv_report import CSV
+# data should be in the format of 2D array or list of lists
+output = CSV(data, ['Header 1', 'Header 2', 'Header 3', 'Header 4'], output_file='name_of_output_file.csv')
+output.generate_csv()
+
+
+
 '''
 
 import csv
@@ -34,6 +46,11 @@ class CSV(object):
 			writer = csv.writer(csv_file)
 			writer.writerow(self.list_of_headers)
 			for row in self.csv_data[number_of_splits * self.row_limit : (number_of_splits + 1) * self.row_limit]:
+				# This loop looks for unicode objects and encodes them to ASCII to avoif Unicode errors,
+				# for e.g. UnicodeEncodeError: 'ascii' codec can't encode character u'\xf1'
+				for index,item in enumerate(row[:]):
+				    if type(item) is unicode:
+					row[index] = item.encode('ascii', 'ignore')
 				writer.writerow(row)
 
 	def __len__(self):
