0d779c7ae5df1461a2d3c8dcf32297fdb5212cac
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-09-22 16:18:32 -0400

    Create forum_stats.py

---
+++ reporting_scripts/forum_stats.py
@@ -0,0 +1,23 @@
+'''
+This module calculates the number of forum threads and posts for a given course
+stored in the MongoDB database
+
+Usage:
+
+python <path_to_script> 
+
+'''
+from base_edx import EdXConnection
+
+connection = EdXConnection('forum' )
+collection = connection.get_access_to_collection()
+
+# Number of documents with _type CommentThread
+# A CommentThread represents the first level of interaction: a post that opens 
+#a new thread, often a student question of some sort
+number_of_comment_threads = collection['forum'].find({'_type' : 'CommentThread'}).count()
+
+# Total number of comments in the forum
+number_of_posts = collection['forum'].find().count()
+
+print number_of_posts, number_of_comment_threads
