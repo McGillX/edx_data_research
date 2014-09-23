be353d11c0f854a7199b58587c2dbad9be1549e1
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-09-23 12:23:45 -0400

    Update session_info.py to use generate_csv_report module

--- reporting_scripts/session_info.py
+++ reporting_scripts/session_info.py
@@ -2,58 +2,52 @@
 This modules gathers the session time for each user everytime they logged in i.e. how long
 did they stay logged in
 
+Usage:
+
+python session_info.py
+
 '''
-from pymongo import MongoClient
-from pprint import pprint
 import csv
 from datetime import datetime
-DATABASE_ADDRESS = "mongodb://localhost"
-DATABASE_NAME = 'edx'
-DATABASE_TRACKING_COLLECTION = 'tracking'
 
-client = MongoClient(DATABASE_ADDRESS)
-db = client[DATABASE_NAME]
-tracking = db[DATABASE_TRACKING_COLLECTION]
-#query = tracking.find({"session" : "a30aa421a768a4c75c4ce0156a540e20"})
-query = tracking.find()
-#query = tracking.find({"username" : ""})
+from base_edx import EdXConnection
+from generate_csv_report import CSV
+
+connection = EdXConnection('tracking_atoc185x')
+collection = connection.get_access_to_collection()
+
+query = collection['tracking_atoc185x'].find()
 users_to_sessions = {}
 fail = []
 for index, item in enumerate(query):
-    #print index
     try:
         if 'session' in item:
             if item["username"] in users_to_sessions:
-	            if item["session"] in users_to_sessions[item["username"]]:
-	               users_to_sessions[item["username"]][item['session']].append(item["time"])
+                if item["session"] in users_to_sessions[item["username"]]:
+                   users_to_sessions[item["username"]][item['session']].append(item["time"])
                 else:
-	               users_to_sessions[item["username"]][item['session']] = [item["time"]]
-	    else:
-	        users_to_sessions[item["username"]]  = {}
-	        users_to_sessions[item["username"]][item['session']] = [item["time"]]
+                   users_to_sessions[item["username"]][item['session']] = [item["time"]]
+            else:
+                users_to_sessions[item["username"]]  = {}
+                users_to_sessions[item["username"]][item['session']] = [item["time"]]
     except:
-	   print "Fail -> %s" %item
+        print "Fail -> %s" %item
         fail.append(item)
 
-print "Number of fail: " + len(fail)
+print "Number of fail: " + str(len(fail))
 if fail:
     import json
     with open('report.txt', 'w') as outfile:
         json.dump(fail, outfile)
 else:
     print "no fail"
-with open("session_info.csv", 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(['Username', 'Session ID', 'Number of Events', 'Start Time', 'End Time', 'Time Spent'])
-    for i in users_to_sessions:
-    	#data = [i]
-	for j in users_to_sessions[i]:
-            #print j, len(users_to_sessions[i][j])
-	    max_time = max(users_to_sessions[i][j])
-            #print max_time
-            end_time  = datetime.strptime(max_time.split('+')[0], "%Y-%m-%dT%H:%M:%S.%f")
-            min_time = min(users_to_sessions[i][j])
-            #print min_time
-            start_time = datetime.strptime(min_time.split('+')[0], "%Y-%m-%dT%H:%M:%S.%f")
-            #print "Time difference: " + str(a - b)	
-	    writer.writerow([i, j, len(users_to_sessions[i][j]), start_time, end_time, end_time - start_time])
+result = []
+for item in users_to_sessions:
+    for nested_item in users_to_sessions[item]:
+        max_time = max(users_to_sessions[item][nested_item])
+        end_time = datetime.strptime(max_time.split('+')[0], "%Y-%m-%dT%H:%M:%S.%f")
+        min_time = min(users_to_sessions[item][nested_item])
+        start_time = datetime.strptime(min_time.split('+')[0], "%Y-%m-%dT%H:%M:%S.%f")
+        result.append([item, nested_item, len(users_to_sessions[item][nested_item]), start_time, end_time, end_time - start_time])
+output = CSV(result, ['Username', 'Session ID', 'Number of Events', 'Start Time', 'End Time', 'Time Spent'], output_file='session_info.csv')
+output.generate_csv()
