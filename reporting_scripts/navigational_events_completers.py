cf6de249d4b5af72bdaff6b2e84502f8a5e17e31
ueg1990 <uehtesham90@gmail.com>
2014-10-07 22:45:13 -0400

    Update reporting_scripts/navigational_events_completers.py

--- reporting_scripts/navigational_events_completers.py
+++ reporting_scripts/navigational_events_completers.py
@@ -6,8 +6,9 @@ for those students who completed the course
 import csv
 
 from base_edx import EdXConnection
+from generate_csv_report import CSV
 
-connection = EdXConnection('tracking')
+connection = EdXConnection('tracking_atoc185x')
 collection = connection.get_access_to_collection()
 
 with open('csv_files/McGillX_CHEM181x_1T2014_grade_report_2014-04-24-1030.csv', 'r') as csv_file:
@@ -15,13 +16,23 @@ with open('csv_files/McGillX_CHEM181x_1T2014_grade_report_2014-04-24-1030.csv', 
     usernames = [row[2] for row in reader]
 
 
-cursor = collection['tracking'].aggregate([{"$match" : {"event_source" : "browser", "$or" : [{"event_type" : "seq_prev"},{"event_type" : "seq_goto"},{"event_type" : "seq_next"}], 'username' : {'$in' : usernames}}}, {"$group" : {"_id" : {'chapter_name' : "$parent_data.chapter_display_name", "display_name" :  "$metadata.display_name", "event_type"  : "$event_type", "event_old" : "$event.old", "event_new" : "$event.new"}, "count" : {"$sum" : 1}}}])
+cursor = collection['tracking_atoc185x'].aggregate([{"$match" : {"event_source" : "browser", "$or" : [{"event_type" : "seq_prev"},{"event_type" : "seq_goto"},{"event_type" : "seq_next"}], 'username' : {'$in' : usernames}}}, {"$group" : {"_id" : {'chapter_name' : "$parent_data.chapter_display_name", "display_name" :  "$metadata.display_name", "event_type"  : "$event_type", "event_old" : "$event.old", "event_new" : "$event.new"}, "count" : {"$sum" : 1}}}])
 
-with open('csv_files/navigation_frequency_completers.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(['Chapter Name', 'Display Name', 'Event Type', 'Event Old', 'Event New', 'Count'])
-    for item in cursor['result']:
-        try:
-            writer.writerow([item['_id']['chapter_name'], item['_id']['display_name'], item['_id']['event_type'], item['_id'].get('event_old', 0), item['_id']['event_new'], item['count']])
-        except:
-           pass 
+#with open('csv_files/navigation_frequency_completers.csv', 'w') as csv_file:
+#    writer = csv.writer(csv_file)
+#    writer.writerow(['Chapter Name', 'Display Name', 'Event Type', 'Event Old', 'Event New', 'Count'])
+#    for item in cursor['result']:
+#        try:
+#            writer.writerow([item['_id']['chapter_name'], item['_id']['display_name'], item['_id']['event_type'], item['_id'].get('event_old', 0), item['_id']['event_new'], item['count']])
+#        except:
+#           pass 
+
+result = []
+for item in cursor['result']:
+    try:
+        result.append([item['_id']['chapter_name'], item['_id']['display_name'], item['_id']['event_type'], item['_id'].get('event_old', 0), item['_id']['event_new'], ite    m['count']])
+    except:
+        pass
+
+output = CSV(result, ['Chapter Name', 'Display Name', 'Event Type', 'Event Old', 'Event New', 'Count'], output_file='navigation_frequency_completers.csv')
+output.generate_csv()
