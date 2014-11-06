e58425ea00d1a138506527f72241e5d377f51e6b
ueg1990 <uehtesham90@gmail.com>
2014-11-05 21:23:23 -0500

    Fix bug in entrance_exit_surveys.py

--- reporting_scripts/surveys/entrance_exit_surveys.py
+++ reporting_scripts/surveys/entrance_exit_surveys.py
@@ -43,20 +43,28 @@ import json
 from base_edx import EdXConnection
 from generate_csv_report import CSV
 
-connection = EdXConnection('courseware_studentmodule', 'auth_user')
+connection = EdXConnection('a_courseware_studentmodule', 'a_auth_user')
+#connection = EdXConnection('courseware_studentmodule', 'auth_user')
 collection = connection.get_access_to_collection()
 
 # Modify key-value pairs in survey_pages to the name of the survey pages and to 
 # the problem ids that maps to the survey pages E.g. if a course have a 
-survey_pages = {'entrance_survey' : {'general_info' : 'i4x://McGillX/ATOC185x/problem/e60f566b9a9342ac9b8dd3f92296af41', 
-'demographics_background' : 'i4x://McGillX/ATOC185x/problem/8781ed8818064bf08722ee3175c2f356' , 
-'aspirations_motivation' : 'i4x://McGillX/ATOC185x/problem/ca3486d4d1ef49ea8c6aa38534bab855'}, 
-'exit_survey' : {'part_1' : 'i4x://McGillX/ATOC185x/problem/7620c0262e3d44049d73ba5fed62edfd',
-'part_2': 'i4x://McGillX/ATOC185x/problem/c993861c76e5484d8233e702af2e4b3d'}}
+
+#survey_pages = {'entrance_survey' : {'general_info' : 'i4x://McGillX/ATOC185x/problem/e60f566b9a9342ac9b8dd3f92296af41', 
+#'demographics_background' : 'i4x://McGillX/ATOC185x/problem/8781ed8818064bf08722ee3175c2f356' , 
+#'aspirations_motivation' : 'i4x://McGillX/ATOC185x/problem/ca3486d4d1ef49ea8c6aa38534bab855'}, 
+#'exit_survey' : {'part_1' : 'i4x://McGillX/ATOC185x/problem/7620c0262e3d44049d73ba5fed62edfd',
+#'part_2': 'i4x://McGillX/ATOC185x/problem/c993861c76e5484d8233e702af2e4b3d'}}
+
+survey_pages = {'entrance_survey' : {'general_info' : 'i4x://McGillX/CHEM181x/problem/c9d2efffbdf043e68789bd60cd4954e3', 
+'demographics_background' : 'i4x://McGillX/CHEM181x/problem/134cfc9efb2b400bab2ee1505cc9e4a9' , 
+'aspirations_motivation' : 'i4x://McGillX/CHEM181x/problem/579ae070227c4f5c973eb02affdcba2a'}, 
+'exit_survey' : {'part_1' : 'i4x://McGillX/CHEM181x/problem/72c6a513dae945779520c3a93bb5bc49',
+'part_2': 'i4x://McGillX/CHEM181x/problem/0ec10c7420a040f6beb2be520fe1eb50'}}
 
 survey_ids = [_id for page in survey_pages.values() for _id in page.values()]
-cursor_courseware_studentmodule = collection['courseware_studentmodule'].find()
-cursor_student = collection['auth_user']
+cursor_courseware_studentmodule = collection['a_courseware_studentmodule'].find()
+cursor_student = collection['a_auth_user']
 
 # For each student, get the values filled in a survey page and store the results
 # in a dictionary where the key is the username. The username is extracted from
@@ -76,28 +84,31 @@ for document in cursor_courseware_studentmodule:
 # For loop to retrieve the names of all the survey pages. Since a student may
 # not have filled all pages, we look for the longest list and use the values
 # to retrieve the survey pages
+survey_question_ids = {}
 for value in result.values():
     if len(value) == 5:
-        survey_question_ids = {key for item in value for key in item.keys()}
-        print survey_question_ids
-        break
-
+        temp = {key for item in value for key in item.keys()}
+        if len(temp) > len(survey_question_ids):
+            survey_question_ids = temp
+        #break
+print survey_question_ids
 survey_question_ids = sorted(list(survey_question_ids))
 csv_data = []
 for username, survey_info in result.iteritems():
     temp = [''] * len(survey_question_ids) 
     for item in survey_info:
         for key,value in item.iteritems():
-            #index = survey_question_ids.index(key)
-            #if key in survey_question_ids:
-            #    temp[index] = value
-            try:
-                index = survey_question_ids.index(key)
-                if key in survey_question_ids:
-                    temp[index] = value
-            except:
+            index = survey_question_ids.index(key)
+            if key in survey_question_ids:
+                temp[index] = value
+            #try:
+            #    index = survey_question_ids.index(key)
+            #    if key in survey_question_ids:
+            #        temp[index] = value
+            #except:
                 # Skip or do something if key not in list of survey question ids
-                pass
+                #print key
+            #    pass
     temp.insert(0, username)
     csv_data.append(temp)
 
---
+++ reporting_scripts/surveys/generate_csv_report.py
@@ -0,0 +1,54 @@
+'''
+This module generates a csv report based on given csv_data, which will be a 2D array, and the list of list_of_headers
+If the data is very big, this module will split the data into a number of csv files. E.g. if the 2D data has 10200 
+rows, and user wants csv file to have only 5000 rows, then this module would split it into 3 csv files with 5000, 5000
+and 200 rows
+
+The CSV constructor has 2 required parameters: csv_data and list_of_headers and 2 optional parameters: output_file
+and row_limit. 
+
+Usage:
+
+from generate_csv_report import CSV
+# data should be in the format of 2D array or list of lists
+output = CSV(data, ['Header 1', 'Header 2', 'Header 3', 'Header 4'], output_file='name_of_output_file.csv')
+output.generate_csv()
+'''
+
+import csv
+from collections import defaultdict
+
+class CSV(object):
+
+	def __init__(self, csv_data, list_of_headers, output_file='output.csv', row_limit=100000):
+		self.csv_data = csv_data
+		self.output_file = output_file
+		self.list_of_headers = list_of_headers
+		self.output_file = output_file
+		self.row_limit = row_limit
+
+	def generate_csv(self):
+		if len(self) <= self.row_limit:
+			self._write_to_csv(self.output_file)
+		else:
+			if len(self) % self.row_limit:
+				number_of_splits = len(self) // self.row_limit + 1
+			else:
+				number_of_splits = len(self) // self.row_limit
+			for index in xrange(number_of_splits):
+				self._write_to_csv(self.output_file.split('.')[0] + '_' + str(index) + '.csv', index)					
+
+	def _write_to_csv(self, output_file, number_of_splits=0):
+		with open(output_file, 'w') as csv_file:
+			writer = csv.writer(csv_file)
+			writer.writerow(self.list_of_headers)
+			for row in self.csv_data[number_of_splits * self.row_limit : (number_of_splits + 1) * self.row_limit]:
+				# This loop looks for unicode objects and encodes them to ASCII to avoif Unicode errors,
+				# for e.g. UnicodeEncodeError: 'ascii' codec can't encode character u'\xf1'
+				for index,item in enumerate(row[:]):
+				    if type(item) is unicode:
+					row[index] = item.encode('ascii', 'ignore')
+				writer.writerow(row)
+
+	def __len__(self):
+		return len(self.csv_data) + 1
