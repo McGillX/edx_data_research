fd097756c5a3bc30972c004deece4b03f2e9fedc
ueg1990 <uehtesham90@gmail.com>
2014-11-21 01:22:44 -0500

    Update user_info.py

--- reporting_scripts/user_info.py
+++ reporting_scripts/user_info.py
@@ -15,15 +15,17 @@ from generate_csv_report import CSV
 connection = EdXConnection('certificates_generatedcertificate', 'auth_userprofile')
 collection = connection.get_access_to_collection()
 
-documents = collection[].find()
+documents = collection['auth_userprofile'].find()
 
 result = []
 
 for document in documents:
-    user_id = document['id']
-    final_grade = collection['certificates_generatedcertificate'].find_one({'id' : user_id})['grade']
+    user_id = document['user_id']
+    try:
+        final_grade = collection['certificates_generatedcertificate'].find_one({'user_id' : user_id})['grade']
+    except:
+        print user_id
     result.append([user_id, document['name'], final_grade, document['gender'], document['year_of_birth'], document['level_of_education'], document['country'], document['city']])
     
-
-
-
+output = CSV(result, ['User ID','Username', 'Final Grade', 'Gender', 'Year of Birth', 'Level of Education', 'Country', 'City'], output_file='atoc185x_user_info.csv')
+output.generate_csv()
