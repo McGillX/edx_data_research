4fbfcce8d369db919df53b85cda198eded66fdc7
ueg1990 <uehtesham90@gmail.com>
2014-11-26 23:58:47 -0500

    Start tasks to find chapters accessed per user

---
+++ reporting_scripts/chapters_accessed_per_user.py
@@ -0,0 +1,19 @@
+'''
+This module determines how many chapters were accessed by each user for a 
+given course
+
+Usage:
+
+python chapters_accessed_per_user
+
+'''
+
+
+from base_edx import EdXConnection
+from generate_csv_report import CSV 
+
+connection = EdXConnection('tracking', 'course_structure')
+collection = connection.get_access_to_collection()
+
+chapters = collection['course_structure'].distinct('parent_data.chapter_display_name')
+
