2e9527d00358027a3f85d3087734ab4e87441fc4
ueg1990 <uehtesham90@gmail.com>
2014-11-26 23:58:07 -0500

    Update to handle users with no final grade

--- reporting_scripts/user_info.py
+++ reporting_scripts/user_info.py
@@ -23,9 +23,10 @@ for document in documents:
     user_id = document['user_id']
     try:
         final_grade = collection['certificates_generatedcertificate'].find_one({'user_id' : user_id})['grade']
+        result.append([user_id, document['name'], final_grade, document['gender'], document['year_of_birth'], document['level_of_education'], document['country'], document['city']])
     except:
-        print user_id
-    result.append([user_id, document['name'], final_grade, document['gender'], document['year_of_birth'], document['level_of_education'], document['country'], document['city']])
+        # Handle users with no grades
+        pass
     
 output = CSV(result, ['User ID','Username', 'Final Grade', 'Gender', 'Year of Birth', 'Level of Education', 'Country', 'City'], output_file='atoc185x_user_info.csv')
 output.generate_csv()
