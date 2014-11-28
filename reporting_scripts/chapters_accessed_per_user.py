2a64b034e6d92763d2c363637bed2b094daa5283
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-11-27 23:51:58 -0500

    Update chapters_accessed_per_user.py

--- reporting_scripts/chapters_accessed_per_user.py
+++ reporting_scripts/chapters_accessed_per_user.py
@@ -7,7 +7,7 @@ Usage:
 python chapters_accessed_per_user
 
 '''
-
+from collections import defaultdict
 
 from base_edx import EdXConnection
 from generate_csv_report import CSV 
@@ -17,4 +17,13 @@ collection = connection.get_access_to_collection()
 
 # Get all chapters
 chapters = collection['course_structure'].distinct('parent_data.chapter_display_name')
+
+tracking = collection['tracking'].find()
+result = []
+for document in tracking:
+    if 'parent_data' in document:
+        pass
+    
+output = CSV(result, ['Username'].extend(chapters), output_file='atoc185x_chapters_accesses_per_user.csv')
+output.generate_csv()
 
