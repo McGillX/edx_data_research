2e47275343494ab0e2d28c9596f0cb3f1ede0f11
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-11-30 23:43:03 -0500

    Update load_tracking_logs_to_mongo.py

--- parsing/tracking_logs/load_tracking_logs_to_mongo.py
+++ parsing/tracking_logs/load_tracking_logs_to_mongo.py
@@ -8,3 +8,31 @@ tracking log collection in the course specific database
 Tracking logs will have an extention of either .log or .log.gz
 
 '''
+
+import pymongo
+import json
+import glob
+import sys
+import gzip
+import datetime
+import os
+
+ERRORFILE_SUFFIX = "-errors"
+
+def connect_to_db_collection(db, collection):
+    pass
+
+def get_course_id(event):
+    pass
+
+def canonical_name(filepath):
+    pass
+
+def migrate_tracking_logs_to_mongo():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
