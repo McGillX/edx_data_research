6a78b63654128443375f442479e3738ba23802fa
Usman Ehtesham Gul <uehtesham90@gmail.com>
2014-12-01 21:20:56 -0500

    Update load_tracking_logs_to_mongo.py

--- parsing/tracking_logs/load_tracking_logs_to_mongo.py
+++ parsing/tracking_logs/load_tracking_logs_to_mongo.py
@@ -20,13 +20,48 @@ import os
 ERRORFILE_SUFFIX = "-errors"
 
 def connect_to_db_collection(db, collection):
-    pass
+    db_name = db
+    collection_name = collection
+    
+    # Get database connection and collections
+    connection = pymongo.Connection('localhost', 27017)
+    db = connection[db_name]
+    tracking_collection = db[collection_name]
+    imported_collection = db[collection_name + "_imported"]
+    return tracking_collection, imported_collection
 
 def get_course_id(event):
-    pass
+    """
+    Try to harvest course_id from various parts of an event.  Assumes that
+    the "event" has already been parsed into a structure, not a json string.
+    The course_id should be of the format A/B/C and cannot contain dots.
+    """
+    course_id = None
+    if event['event_source'] == 'server':
+        # get course_id from event type
+        if event['event_type'] == '/accounts/login/':
+            s = event['event']['GET']['next'][0]
+        else:
+            s = event['event_type']
+    else:
+        s = event['page']
+    if s:
+        a = s.split('/')
+        if 'courses' in a:
+            i = a.index('courses')
+            if (len(a) >= i+4):
+                course_id = "/".join(a[i+1:i+4]).encode('utf-8').replace('.','')
+    return course_id
 
 def canonical_name(filepath):
-    pass
+    """
+    Save only the filename and the subdirectory it is in, strip off all prior 
+    paths.  If the file ends in .gz, remove that too.  Convert to lower case.
+    """
+    fname = '/'.join(filepath.lower().split('/')[-2:])
+    if len(fname) > 3 and fname[-3:] == ".gz":
+        fname = fname[:-3]
+    return fname
 
 def migrate_tracking_logs_to_mongo():
     pass
