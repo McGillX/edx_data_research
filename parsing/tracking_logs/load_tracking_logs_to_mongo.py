4433ef64565bd06129d7861f43606f9444a92145
ueg1990 <uehtesham90@gmail.com>
2014-12-02 23:37:46 -0500

    Load content from logs

--- parsing/tracking_logs/load_tracking_logs_to_mongo.py
+++ parsing/tracking_logs/load_tracking_logs_to_mongo.py
@@ -17,7 +17,7 @@ import gzip
 import datetime
 import os
 
-ERRORFILE_SUFFIX = "-errors"
+ERROR_FILE_SUFFIX = "-errors"
 
 def connect_to_db_collection(db, collection):
     db_name = db
@@ -55,7 +55,7 @@ def get_course_id(event):
 
 def canonical_name(filepath):
     """
-    Save only the filename and the subdirectory it is in, strip off all prior 
+    Save only the filename and the subdirectory it is in, strip off all prior
     paths.  If the file ends in .gz, remove that too.  Convert to lower case.
     """
     fname = '/'.join(filepath.lower().split('/')[-1:])
@@ -76,7 +76,8 @@ def get_tracking_logs(path_to_logs):
             logs.append(item)
         elif os.path.isdir(item):
             for (dir_path, dir_names, file_names) in os.walk(item):
-                logs.extend(file_names)
+                for name in file_names:
+                    logs.append(os.path.join(dir_path, name))
     return logs
     
 def migrate_tracking_logs_to_mongo(tracking, tracking_imported):
@@ -86,14 +87,32 @@ def main():
     if len(sys.argv) < 4:
         usage_message = """usage: %s db coll f1 [f2] [f3...]
             For one or more files containing edx tracking logs, insert into the
-            collection given. Files ending .gz they are decompressed on the fly.
-            Files successfully loaded are tracked in coll_incremental. If already
-            loaded, skip.
+            collection given. Files ending .gz they are decompressed on the fly
+            Files successfully loaded are tracked in tracking_imported. If 
+            already loaded, skip.
             """
         sys.stderr.write(usage_message % sys.argv[0])
         sys.exit(1)
         
-    tracking, tracking_imported = connect_to_db_collection(sys.argv[1], sys.arv[2])
+    tracking, tracking_imported = connect_to_db_collection(sys.argv[1], sys.argv[2])
+    log_files = get_tracking_logs(sys.argv[3:]) 
+    for log in sorted(log_files):
+        log_file_name = canonical_name(log)
+        if tracking_imported.find({'_id' : log_file_name}) or log_file_name.endswith(ERROR_FILE_SUFFIX): 
+            print "Log file {0} already loaded".format(log)
+            continue
+        print "Loading log file {0}".format(log)
+        if log.endswith('.gz'):
+            file_handler = gzip.open(log)
+            log_content = file_handler.readlines()
+            file_handler.close()
+            event_source = log_file_name[:-3]
+            error_file_name = event_source + ERROR_FILE_SUFFIX
+        else:
+            with open(log) as file_handler:
+                log_content = file_handler.readlines()
+            event_source = log_file_name
+            error_file_name = event_source + ERROR_FILE_SUFFIX
 
 if __name__ == '__main__':
     main()
