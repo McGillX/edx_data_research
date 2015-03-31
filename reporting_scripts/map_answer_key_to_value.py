0163b3b0eebb868a7bebc54f9f77c4c2868bf27f
ueg1990 <uehtesham90@gmail.com>
2015-03-31 01:54:29 -0400

    Update script to save answers to a csv reports

--- reporting_scripts/map_answer_key_to_value.py
+++ reporting_scripts/map_answer_key_to_value.py
@@ -1,7 +1,6 @@
 import csv
 import sys
-
-from collections import defaultdict
+import json
 
 # These modules can be found under reporting scripts. You will have to add them
 # in the same directory as this script
@@ -32,10 +31,47 @@ for row in rows:
             answer_keys[problem_id].update(dict_value_answer)
         except:
             answer_keys[problem_id] = dict_value_answer
-         
 student_answers_db = collection['tracking'].find({'event_type': 'problem_check', 'event_source' : 'server'})
+output = dict()
 for doc in student_answers_db:
     try:
-        print doc['event']['state']['student_answers']
+        username = doc["username"]
+        problem_id = doc['event']['problem_id']
+        student_answers =  doc['event']['answers']
+        student_attempts =  doc['event']['attempts']
+        student_answers_values = dict()
+        for key, values in student_answers.iteritems():
+            if isinstance(values, list):
+                if "choice_" in ",".join(values):
+                    mapped_values = answer_keys[key].values()
+                    student_answers_values[key] = mapped_values
+                else:
+                    student_answers_values[key] = values 
+            else:
+                if "choice_" in values:
+                    mapped_values = answer_keys[key].values()
+                    student_answers_values[key] = mapped_values
+                else:
+                    student_answers_values[key] = values
+        if username not in output:
+            output[username] = {'problem_id' : problem_id, 'student_answers' :  student_answers_values, 'attempts' : student_attempts}
+        else: 
+            if student_attempts > output[username]['attempts']:
+                output[username]['problem_id'] = problem_id
+                output[username]['student_answers'] = student_answers_values
+                output[username]['attempts'] = student_attempts
     except:
-        print "Key Error!!!"
+        print "Key Error!!!", key, values, problem_id
+
+result = []
+for key, values in output.iteritems():
+    username = key
+    problem_id = values['problem_id']
+    for question, answer in values['student_answers'].iteritems():
+        result.append([username, problem_id, question, answer])
+        
+output = CSV(result, ['Username','Problem ID', 'Student Answers' ], output_file=db_name + '_student_answers.csv')
+output.generate_csv()
+
+#with open(db_name + '_student_answers', 'w') as f:
+#    json.dump(output, f)
