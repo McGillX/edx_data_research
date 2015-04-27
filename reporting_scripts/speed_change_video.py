d6a4be56d606632b1d4f9c465f974f9778e7c438
Alexander Steeves-Fuentes <alexander.steeves-fuentes@mcgill.ca>
2015-04-27 13:53:09 -0400

    Update speed_change_video.py

--- reporting_scripts/speed_change_video.py
+++ reporting_scripts/speed_change_video.py
@@ -1,9 +1,9 @@
 '''
-This module gets all the events per user while watching videos. Since we will
-need to sort a very large number of documents, user should create a separate
-collection to aggregate all required documents in one collection and then 
-extract results from the new collection
-Command to run on the mongo shell to creare new collection:
+This module gets all the events per user while watching videos. 
+Since we will need to sort a very large number of documents, you should create a separate collection to 
+aggregate all required documents in one collection and then extract results from the new collection.
+
+Command to run on the mongo shell to create new collection:
 
 db.tracking_atoc185x.aggregate([{$match : {$and : [{"event_type" : "speed_change_video"},{ "parent_data": { $exists: true } }]}}, {$sort : {"parent_data.chapter_display_name" : 1, "parent_data.sequential_display_name" : 1, "parent_data.vertical_display_name" : 1}}, {$out : "speed_change_video_data"}], {allowDiskUse : true})
 
