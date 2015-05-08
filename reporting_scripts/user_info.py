431c33520b6b9e7e2f35c8e2d8aec4f348718c89
AlexanderFuentes <alexander.steeves-fuentes@mcgill.ca>
2015-05-08 14:55:09 -0400

    added username to output file

--- reporting_scripts/user_info.py
+++ reporting_scripts/user_info.py
@@ -14,7 +14,7 @@ from generate_csv_report import CSV
 
 db_name = sys.argv[1]
 
-connection = EdXConnection(db_name, 'certificates_generatedcertificate', 'auth_userprofile')
+connection = EdXConnection(db_name, 'certificates_generatedcertificate', 'auth_userprofile','user_id_map')
 collection = connection.get_access_to_collection()
 documents = collection['auth_userprofile'].find()
 
@@ -23,10 +23,11 @@ for document in documents:
     user_id = document['user_id']
     try:
         final_grade = collection['certificates_generatedcertificate'].find_one({'user_id' : user_id})['grade']
-        result.append([user_id, document['name'], final_grade, document['gender'], document['year_of_birth'], document['level_of_education'], document['country'], document['city']])
+        username = collection['user_id_map'].find_one({'id' : user_id})['username']
+        result.append([user_id, document['name'], final_grade, username, document['gender'], document['year_of_birth'], document['level_of_education'], document['country'], document['city']])
     except:
         # Handle users with no grades
         pass
     
-output = CSV(result, ['User ID','Username', 'Final Grade', 'Gender', 'Year of Birth', 'Level of Education', 'Country', 'City'], output_file=db_name+'-user_info.csv')
+output = CSV(result, ['User ID','Name', 'Final Grade', 'Username', 'Gender', 'Year of Birth', 'Level of Education', 'Country', 'City'], output_file=db_name+'-user_info.csv')
 output.generate_csv()
