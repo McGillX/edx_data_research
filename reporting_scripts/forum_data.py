04eae79deb30e63f68534784f1eaf0412cfb1aa9
ueg1990 <uehtesham90@gmail.com>
2015-05-14 00:13:51 -0400

    Update script with latest fields

--- reporting_scripts/forum_data.py
+++ reporting_scripts/forum_data.py
@@ -14,7 +14,7 @@ collection = connection.get_access_to_collection()
 forum_data = collection['forum'].find() 
 csv_data = []
 for document in forum_data:
-    csv_data.append([document['_id']['oid'], document['author_username'], document['_type'], document.get('title', ''), document['body'], document['created_at']['date']])
+    csv_data.append([document['_id'], document['author_username'], document['_type'], document.get('title', ''), document['body'], document['created_at']])
 
 headers = ['ID', 'Author Username', 'Type', 'Title', 'Body', 'Created At Date']
 output = CSV(csv_data, headers, output_file=db_name+'_forum_data.csv')
