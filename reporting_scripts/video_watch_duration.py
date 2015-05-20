b37f4e88cfb1463905d474a7e22899395947516d
Alexander Steeves-Fuentes <AlexanderFuentes@users.noreply.github.com>
2015-05-20 16:09:44 -0400

    Update video_watch_duration.py

--- reporting_scripts/video_watch_duration.py
+++ reporting_scripts/video_watch_duration.py
@@ -1,36 +1,13 @@
 '''
+-get all the distinct usernames
 
-1) Create a new collection in the mongo shell:
+-get every tracking event associated with a username and sort them oldest to newest -chronologically
 
-db.tracking_collection.aggregate([{$match :{$or : [{"event_type" : "load_video"},{"event_type" : "seek_video"}, {"event_type" : "play_video"},{"event_type":"pause_video"}]}}, {$out : "video_watch_duration_collection"}])
+-loop through the events for the user
+--if it finds a play_video event that becomes the watch session start
+--next event becomes the watch session end 
 
-2) Run: python video_watch_segments.py
-
-
-desired final output.csv
-
-new row for every unique load_video event for a username
-
-username, video associated with load_video event, parent_data: {chapter_display_name, sequential_display_name, vertical_display_name,}, edx_video_id, video watch segments
-
-get the event_types : load_video, play_video, pause_video, seek_video
-
-sort by "time": "" so that the events are chronologically ordered
-
-for each load_video new video watch segment should include ONLY:
-
-- time between play_video -> next video event in time (pause_video or seek_video)
-- time between seek_video : {'new_time' : Time}  -> pause video (only with new_time > old_time, this is to avoid including rewinds)
-
-watch periods:
-    event_type : pause_video - "event_type":"play_video" {"event":{"currentTime":TIME}} = new video watch segment
-    if seek_video : {'old_time' : TIME} < seek_video : {'new_time' : TIME} 
-      "pause_video" {"event":{"currentTime":TIME}} - seek_video : {'new_time': TIME } = new video watch segment
-
-    if seek_video : {'old_time' : TIME} > seek_video : {'new_time' : TIME} = rewind (exclude from watch segments)
-    
-Notes:
-Do not use session key from logs, only ends when student's log out
+later version to consider all the pause, skip possibilities
     
 '''
 import sys
