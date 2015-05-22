e04a97f6de1e2c3d17cd72807ad108709dbd32d5
Alexander Steeves-Fuentes <AlexanderFuentes@users.noreply.github.com>
2015-05-22 11:48:14 -0400

    Update video_watch_duration.py

--- reporting_scripts/video_watch_duration.py
+++ reporting_scripts/video_watch_duration.py
@@ -1,6 +1,7 @@
 '''
--start by aggregating a new collection:
+1) start by aggregating a new collection that includes the video interaction, page navigation and page close events for users sorted by username and time
 db.tracking.aggregate([{ $limit : 30 },{$match:{$and:[{'event_source':'browser'},{$or:[{'event_type':'play_video'},{'event_type':'speed_change_video'},{'event_type':'seq_goto'}, {'event_type':'seq_next'}, {'event_type':'seq_prev'}, {'event_type':'page_close'}, {'event_type':'play_video'},{'event_type':'pause_video'}, {'event_type':'seek_video'}, {'event_type':'pause_video'}]}]}}, {$project:{"username":1, "event_type":1, 'time':1,"event":1}},{ $sort: {'username':1,'time':1}},{$out: "new_test_collection"}])
+
 
 -get all the distinct usernames
 
