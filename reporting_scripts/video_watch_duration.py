4aeb34505055dc846a961beb26f49a8e609ff1a8
Alexander Steeves-Fuentes <AlexanderFuentes@users.noreply.github.com>
2015-05-25 11:40:02 -0400

    Update video_watch_duration.py

--- reporting_scripts/video_watch_duration.py
+++ reporting_scripts/video_watch_duration.py
@@ -4,12 +4,12 @@ db.tracking.aggregate([{ $limit : 30 },{$match:{$and:[{'event_source':'browser'}
 '''
 
 import sys
-#import csv
+import csv
 import time
 from datetime import datetime
 
 from base_edx import EdXConnection
-#from generate_csv_report import CSV 
+from generate_csv_report import CSV 
 
 db_name = sys.argv[1]
 
@@ -35,12 +35,19 @@ for student in students[0:2]:
     for index,item in enumerate(cursor):
 #if both a start and end event have been found append them to the list
         if start_event_time !={'blank'} and end_event_time != {'blank'}:
-            duration = start_event_time - end_event_time
-            watch_durations.append([student, video_id, start_event_time,end_event_time])
+            duration = end_event_time - start_event_time
+#append all the values for a given watch_duration to the watch_durations list
+            watch_durations.append([student, video_id, video_code, time_point, start_event_time,end_event_time,duration])
+            start_event_time = {'blank'}
+            end_event_time = {'blank'}
 #if event is play_video then assign as start
         if item['event_type'] == 'play_video':
             start_event_time = datetime.strptime(item['time'].split('+')[0], "%Y-%m-%dT%H:%M:%S.%f")
-            video_id = item['event']
+#get values related to video identification
+            video = item['event']
+            video_code = video['code']
+            video_id = video['id']
+            time_point = video['currentTime']
             print 'start'
             print (start_event_time)
 #when a start event is found set the end event to blank
@@ -55,5 +62,5 @@ for student in students[0:2]:
             continue
 print watch_durations
 
-output = CSV(watch_durations, ['Username','video_id','start_event_time','end_event_time'] output_file='video_watch_duration.csv', row_limit=200000) 
+output = CSV(watch_durations, ['Username','video_id','youtube id (video_code)','time_point (seconds)','start_event_time','end_event_time', 'duration (minutes)'], output_file=db_name+'video_watch_duration.csv', row_limit=200000) 
 output.generate_csv()
