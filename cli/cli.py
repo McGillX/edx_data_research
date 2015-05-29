210581cfef3d54b055ec9f9b1dc6d19b757a4d6e
ueg1990 <uehtesham90@gmail.com>
2015-05-29 02:21:22 -0400

    Add cmd for getting version

---
+++ cli/cli.py
@@ -0,0 +1,5 @@
+import argparse
+
+parser = argparse.ArgumentParser(prog='moocx', description='EdX MOOC Data Anaylysis')
+parser.add_argument('-v', '--version', action='version', version='0.1.0')
+parser.parse_args()
