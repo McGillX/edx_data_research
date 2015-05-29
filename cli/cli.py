6593645ace6efdc0e7b79dbdf5a5b5f76396c693
ueg1990 <uehtesham90@gmail.com>
2015-05-29 03:35:21 -0400

    Add commands for listing available analytics commadns

--- cli/cli.py
+++ cli/cli.py
@@ -2,4 +2,11 @@ import argparse
 
 parser = argparse.ArgumentParser(prog='moocx', description='EdX MOOC Data Anaylysis')
 parser.add_argument('-v', '--version', action='version', version='0.1.0')
+
+subparsers = parser.add_subparsers(help='commands')
+
+# A list command
+list_parser = subparsers.add_parser('list', help='List commands')
+list_parser.add_argument('list_commands', action='store', choices=['all', 'basic'],help='List anayltics commands based on choice')
+
 parser.parse_args()
