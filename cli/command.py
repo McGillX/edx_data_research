5e7838d013dad41e4508e3736d3f2b401b1a0156
Usman Ehtesham Gul <uehtesham90@gmail.com>
2015-06-05 12:34:32 -0400

    Define interface between cli and data analysis

---
+++ cli/command.py
@@ -0,0 +1,22 @@
+"""
+In this module we define the interface between the cli input provided
+by the user and the analytics required by the user
+"""
+
+def cmd_list_basic(args):
+	"""
+	List the basic analytics commands and their summary
+	"""
+	pass
+	
+def cmd_list_all(args):
+	"""
+	List all the analytics commands and their summary
+	"""
+	pass
+	
+def cmd_run_basic(args):
+	"""
+	Run basic analytics
+	"""
+	pass
