11bef7fad394d4a4453a8f6e3e767d68ed291e45
ueg1990 <uehtesham90@gmail.com>
2015-08-20 01:48:23 -0400

    Format code for better PEP8 compatibility

--- edx_data_research/reporting/basic/course_completers.py
+++ edx_data_research/reporting/basic/course_completers.py
@@ -1,15 +1,20 @@
 '''
-This module extracts the student IDs from the collection certificates_generatedcertificate
-of the students who completed the course and achieved a certificate. The ids
-are then used to extract the usernames of the course completers
+This module extracts the student IDs from the collection
+certificates_generatedcertificate of the students who completed the course and
+achieved a certificate. The ids are then used to extract the usernames of the
+course completers
 
 '''
 
 def course_completers(edx_obj):
     edx_obj.collections = ['certificates_generatedcertificate', 'auth_user']
-    cursor = edx_obj.collections['certificates_generatedcertificate'].find({'status' : 'downloadable'})
+    cursor = (edx_obj.collections['certificates_generatedcertificate']
+              .find({'status' : 'downloadable'}))
     result = []
     for item in cursor:
-        user_document = edx_obj.collections['auth_user'].find_one({"id" : item['user_id']})
-        result.append([user_document['id'], user_document['username'], item['name'], item['grade']])
-    edx_obj.generate_csv(result, ['User ID','Username', 'Name', 'Grade'], output_file=edx_obj.db.name+'_course_completers.csv')
+        user_document = (edx_obj.collections['auth_user']
+                        .find_one({"id" : item['user_id']}))
+        result.append([user_document['id'], user_document['username'],
+                       item['name'], item['grade']])
+    edx_obj.generate_csv(result, ['User ID','Username', 'Name', 'Grade'],
+                         output_file=edx_obj.db.name+'_course_completers.csv')
--- edx_data_research/reporting/basic/ip_to_country.py
+++ edx_data_research/reporting/basic/ip_to_country.py
@@ -3,11 +3,12 @@ This module retrieve IP addresses for each student and maps their IP to a countr
 This is to determine the diversity of students who took a given course
 The geoip module and GeoIP.dat file was used to map the IP address to a country
 
-Each user may have multiple ips, so this module retrieves all the countries mapped to those ips
-Disclaimer: The accuracy of the IP to Country cannot be determined as it is difficult to determine
-if the IP is an actual IP or a proxy IP
+Each user may have multiple ips, so this module retrieves all the countries
+mapped to those ips. Disclaimer: The accuracy of the IP to Country cannot be
+determined as it is difficult to determine if the IP is an actual IP or a proxy IP
 
-At the end of the analysis, the results are used to create a Pie Chart to visualize the distribution
+At the end of the analysis, the results are used to create a Pie Chart to
+visualize the distribution
 
 '''
 import csv
@@ -32,7 +33,8 @@ def ip_to_country(edx_obj):
     for key, value_set in tracking.iteritems():
         for value in value_set:
             try:
-                country_code = geoip.country(value, dbname=os.path.join(data_directory, 'GeoIP.dat'))
+                country_code = geoip.country(value, dbname=os.path.join(
+                                             data_directory, 'GeoIP.dat'))
                 country = country_code_to_country[country_code]
                 if not key:
                     key = 'anonymous'
@@ -40,18 +42,24 @@ def ip_to_country(edx_obj):
                 elif (key, country) not in country_set:
                     country_set.add((key,country))
                     result.append([key, value, country_code, country])
-            except:
+            except KeyError:
                 # IMPORTANT
-                # The following code for an exception are hardcoded for those IPs which do have a mapping to a 
-                # country code but they were not available in GeoIP.dat (most probably because it was not updated)
-                # People using this script can either report this code (under except) and or additional conditions
-                # IP addresses which cannot be mapped to a country code stored in GeoIP.dat
+                # The following code for an exception are hardcoded for those
+                # IPs which do have a mapping to a country code but they were
+                # not available in GeoIP.dat (most probably because it was
+                # not updated). People using this script can either report this 
+                # code (under except) and or additional conditions IP addresses
+                # which cannot be mapped to a country code stored in GeoIP.dat
                 if value == '41.79.120.29':
                     country = country_code_to_country['SS']
                     if not key:
                         key = 'anonymous'
-                        result.append([key, value, 'SS', country_code_to_country['SS']])
+                        result.append([key, value, 'SS',
+                                      country_code_to_country['SS']])
                     elif (key, country) not in country_set:
                         country_set.add((key, country))
-                        result.append([key, value, 'SS', country_code_to_country['SS']])
-    edx_obj.generate_csv(result, ['Username', 'IP Address', 'Country Code', 'Country'], output_file=edx_obj.db.name+'_ip_to_country.csv')
+                        result.append([key, value, 'SS',
+                                      country_code_to_country['SS']])
+    edx_obj.generate_csv(result, ['Username', 'IP Address', 'Country Code',
+                         'Country'], output_file=edx_obj.db.name+
+                         '_ip_to_country.csv')
