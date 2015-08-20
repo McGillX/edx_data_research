b02c36bc23af41ee82414f36eb6cf20ffa5a4a46
ueg1990 <uehtesham90@gmail.com>
2015-08-19 20:42:37 -0400

    Add hash_id column to user-info output

--- edx_data_research/reporting/basic/user_info.py
+++ edx_data_research/reporting/basic/user_info.py
@@ -11,9 +11,11 @@ def user_info(edx_obj):
         user_id = item['user_id']
         try:
             final_grade = edx_obj.collections['certificates_generatedcertificate'].find_one({'user_id' : user_id})['grade']
-            username = edx_obj.collections['user_id_map'].find_one({'id' : user_id})['username']
+	    user_id_map = edx_obj.collections['user_id_map'].find_one({'id' : user_id})
+            username = user_id_map['username']
+	    hash_id = user_id_map['hash_id']
             enrollment_date = edx_obj.collections['student_courseenrollment'].find_one({'user_id' : user_id})['created']
-            result.append([user_id, item['name'], final_grade, username, item['gender'], item['year_of_birth'], item['level_of_education'], item['country'], item['city'], enrollment_date])
-        except:
+            result.append([user_id, username, hash_id, item['name'], final_grade, item['gender'], item['year_of_birth'], item['level_of_education'], item['country'], item['city'], enrollment_date])
+        except KeyError:
             print "Exception occurred for user_id {0}".format(user_id)
-    edx_obj.generate_csv(result, ['User ID','Name', 'Final Grade', 'Username', 'Gender', 'Year of Birth', 'Level of Education', 'Country', 'City','Enrollment Date'], output_file=edx_obj.db.name+'_user_info.csv')
+    edx_obj.generate_csv(result, ['User ID', 'Username', 'User Hash ID', 'Name', 'Final Grade', 'Gender', 'Year of Birth', 'Level of Education', 'Country', 'City','Enrollment Date'], output_file=edx_obj.db.name+'_user_info.csv')
