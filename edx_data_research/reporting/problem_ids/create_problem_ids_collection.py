b6b7823e6176dab1cbd72476f37386fc5c82212a
ueg1990 <uehtesham90@gmail.com>
2015-08-19 22:04:59 -0400

    Update problem ids collection generation with user id and hash id

--- edx_data_research/reporting/problem_ids/create_problem_ids_collection.py
+++ edx_data_research/reporting/problem_ids/create_problem_ids_collection.py
@@ -27,16 +27,25 @@ db_name = sys.argv[1]
 # contain the results of this script. Each new document will be inserted into
 # this new collection. The name of the resulting collection could be anything;
 # preferrably relevant to the course
-connection = EdXConnection(db_name, 'tracking','problem_ids')
+connection = EdXConnection(db_name, 'tracking', 'user_id_map', 'problem_ids')
 collection = connection.get_access_to_collection()
 
 cursor = collection['tracking'].find({'event_type' : 'problem_check', 'event_source' : 'server'})
 for document in cursor:
-   doc_result = {}
-   doc_result['username'] = document['username']
-   doc_result['problem_id'] = document['event']['problem_id']
-   doc_result['course_id'] = document['context']['course_id']
-   doc_result['module'] = document['context']['module']
-   doc_result['time'] = document['time']
-   doc_result['event'] = document['event']
-   collection['problem_ids'].insert(doc_result)
+    doc_result = {}
+    username = document['username']
+    if username.isdigit():
+        username = int(username)
+    doc_result['username'] = username
+    user_id_map = collection['user_id_map'].find_one({'username' : username})
+    if not user_id_map:
+        print "Username {0} not found in collection user_id_map".format(username)
+        continue
+    doc_result['user_id'] = user_id_map['id']
+    doc_result['hash_id'] = user_id_map['hash_id']
+    doc_result['problem_id'] = document['event']['problem_id']
+    doc_result['course_id'] = document['context']['course_id']
+    doc_result['module'] = document['context']['module']
+    doc_result['time'] = document['time']
+    doc_result['event'] = document['event']
+    collection['problem_ids'].insert(doc_result)
