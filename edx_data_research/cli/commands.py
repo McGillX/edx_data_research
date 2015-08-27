53c751e86b6ca2dd5076e0a09e7c0e4663c1fcbc
ueg1990 <uehtesham90@gmail.com>
2015-08-26 23:28:21 -0400

    Add function to call problem_ids report generation

--- edx_data_research/cli/commands.py
+++ edx_data_research/cli/commands.py
@@ -5,6 +5,8 @@ by the user and the analytics required by the user
 from edx_data_research.reporting.basic import (user_info, ip_to_country,
                                                course_completers)
 from edx_data_research.reporting.edx_base import EdX
+from edx_data_research.reporting.problem_ids.problem_id import (ProblemId,
+                                                                problem_id)
 
 
 def cmd_list(args):
@@ -21,4 +23,5 @@ def cmd_report_basic(args):
         globals()[args.basic.replace('-', '_')](edx_obj)
 
 def cmd_report_problem_id(args):
-    print 'problem id!!!'
+    edx_obj = ProblemId(args)
+    problem_id(edx_obj)
