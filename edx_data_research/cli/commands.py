b0883ff2b193a4b3d6ee460313af909c6b03a15b
ueg1990 <uehtesham90@gmail.com>
2015-08-26 23:32:36 -0400

    Fix indentation

--- edx_data_research/cli/commands.py
+++ edx_data_research/cli/commands.py
@@ -10,17 +10,17 @@ from edx_data_research.reporting.problem_ids.problem_id import (ProblemId,
 
 
 def cmd_list(args):
-	"""
-	List all the analytics commands and their summary
-	"""
-	print 'list all'
+    """
+    List all the analytics commands and their summary
+    """
+    print 'list all'
 	
 def cmd_report_basic(args):
-	"""
-	Run basic analytics
-	"""
-        edx_obj = EdX(args)
-        globals()[args.basic.replace('-', '_')](edx_obj)
+    """
+    Run basic analytics
+    """
+    edx_obj = EdX(args)
+    globals()[args.basic.replace('-', '_')](edx_obj)
 
 def cmd_report_problem_id(args):
     edx_obj = ProblemId(args)
