753d724b23ca58f585192c669e75f823ff27c987
ueg1990 <uehtesham90@gmail.com>
2015-09-02 18:18:24 -0400

    Update problem_ids to read display_name for given problem id if provided

--- edx_data_research/reporting/edx_problem_ids.py
+++ edx_data_research/reporting/edx_problem_ids.py
@@ -3,7 +3,7 @@ In this module, we will generate a csv report for a given problem id, which
 will include information about how students fared with a given problem id
 
 '''
-from itertools import groupby
+from itertools import groupby, izip_longest
 
 from edx_data_research.reporting.edx_base import EdX
 
@@ -14,13 +14,12 @@ class ProblemIds(EdX):
         super(self.__class__, self).__init__(args)
         self._ids = args.problem_ids
         self.final_attempt = args.final_attempt
+        self.display_names = args.display_names or []
     
     def report_name(self, *args):
         '''Generate name of csv output file from problem id'''
         attempts_name = '-FinalAttempts' if args[2] else '-AllAttempts'
-        display_name = ''
-        if args[1]:
-            display_name = '-' + args[1].replace(':', '').replace(' ', '_')
+        display_name = '-' + args[1].replace(':', '').replace(' ', '_')
         return ('-'.join(arg.lower() for arg in args[0].split('/')[3:]) +
                 display_name + attempts_name + '.csv')
 
@@ -44,10 +43,12 @@ class ProblemIds(EdX):
     def problem_ids(self):
         '''Retrieve information about how students fared for a given problem id'''
         self.collections = ['problem_ids']
-        for problem_id in self._ids:
+        for problem_id, display_name in izip_longest(self._ids,
+                                                     self.display_names,
+                                                     fillvalue=''):
             cursor = self.collections['problem_ids'].find({'event.problem_id' :
                                                           problem_id})
-            display_name = cursor[0]['module']['display_name']
+            display_name = display_name or cursor[0]['module']['display_name']
             one_record = cursor[0]['event']['correct_map']
             problem_id_questions = self.__class__._problem_id_questions(one_record)
             result = []
@@ -63,7 +64,6 @@ class ProblemIds(EdX):
                                          [document['user_id'],
                                           document['username']],
                                          [document['event']['attempts'],
-                                          document['module']['display_name'],
                                           document['time'],
                                           document['event']['success'],
                                           document['event']['grade'],
@@ -77,7 +77,7 @@ class ProblemIds(EdX):
             csv_report_name = self.report_name(problem_id, display_name,
                                                self.final_attempt)
             headers = self.anonymize_row(['Hash ID'], ['User ID', 'Username'],
-                                         ['Attempt Number', 'Module', 'Time',
-                                          'Success', 'Grade Achieved',
-                                          'Max Grade'] + problem_id_questions)
+                                         ['Attempt Number', 'Time', 'Success',
+                                          'Grade Achieved', 'Max Grade'] +
+                                          problem_id_questions)
             self.generate_csv(result, headers, csv_report_name)
