04a41074f09b5cdaaa1b9b2f893c10eebb287a5c
ueg1990 <uehtesham90@gmail.com>
2015-09-03 23:42:39 -0400

    Add code refactoring and formatting

--- edx_data_research/reporting/edx_base.py
+++ edx_data_research/reporting/edx_base.py
@@ -14,16 +14,16 @@ class EdX(object):
         self.output_directory = args.output_directory
         self.row_limit = args.row_limit
         self.csv_data = None
-        self.list_of_headers = None
+        self.headers = None
         self.anonymize = args.anonymize
         
-    def generate_csv(self, csv_data, list_of_headers, output_file):
+    def generate_csv(self, csv_data, headers, output_file):
     	"""
     	Genersate csv report from generated data and given list of headers
     	"""
     	self.csv_data = csv_data
-    	self.list_of_headers = list_of_headers
-    	number_of_rows = len(csv_data) + 1
+        self.headers = headers
+        number_of_rows = len(self.csv_data) + 1
         if number_of_rows <= self.row_limit:
 	        self._write_to_csv(output_file)
         else:
@@ -34,21 +34,20 @@ class EdX(object):
 	        for index in xrange(number_of_splits):
 	            self._write_to_csv(output_file.split('.')[0] + '_' + str(index) + '.csv', index)
 	        
-    def _write_to_csv(self, output_file, number_of_splits=0):
+    def _write_to_csv(self, output_file, index=0):
     	"""
     	Helper method to write rows to csv files
     	"""
     	output_file_path = os.path.abspath(os.path.join(self.output_directory, output_file))
         with open(output_file_path, 'w') as csv_file:
-	        writer = csv.writer(csv_file)
-	        writer.writerow(self.list_of_headers)
-	        for row in self.csv_data[number_of_splits * self.row_limit : (number_of_splits + 1) * self.row_limit]:
-	            # This loop looks for unicode objects and encodes them to ASCII to avoif Unicode errors,
-		        # for e.g. UnicodeEncodeError: 'ascii' codec can't encode character u'\xf1'
-		        for index,item in enumerate(row[:]):
-		            if isinstance(item, unicode):
-		                row[index] = item.encode('ascii', 'ignore')
-		        writer.writerow(row)
+            writer = csv.writer(csv_file)
+            writer.writerow(self.headers)
+            for row in self.csv_data[index * self.row_limit : (index + 1) * self.row_limit]:
+                # This loop looks for unicode objects and encodes them to ASCII to avoif Unicode errors,
+                # for e.g. UnicodeEncodeError: 'ascii' codec can't encode character u'\xf1'
+                row = [item.encode('ascii', 'ignore') for item in row
+                       if isinstance(item, unicode)]
+                writer.writerow(row)
 		
     @property
     def collections(self):
