de99cf5de9fa96c0a5e0853b04070cd4637c43af
ueg1990 <uehtesham90@gmail.com>
2015-09-09 00:36:11 -0400

    Fix bug in csv report generation and add some code refactoring

--- edx_data_research/reporting/edx_base.py
+++ edx_data_research/reporting/edx_base.py
@@ -21,42 +21,49 @@ class EdX(object):
     	"""
     	Genersate csv report from generated data and given list of headers
     	"""
-    	self.csv_data = csv_data
+        self.csv_data = csv_data
         self.headers = headers
-        number_of_rows = len(self.csv_data) + 1
-        if number_of_rows <= self.row_limit:
-	        self._write_to_csv(output_file)
-        else:
-	        if number_of_rows % self.row_limit:
-		        number_of_splits = number_of_rows // self.row_limit + 1
-	        else:
-		        number_of_splits = number_of_rows // self.row_limit
-	        for index in xrange(number_of_splits):
-	            self._write_to_csv(output_file.split('.')[0] + '_' + str(index) + '.csv', index)
+        number_of_data_points = len(self.csv_data)
+        if number_of_data_points < self.row_limit:
+            number_of_rows = self._write_to_csv(output_file)
+            return number_of_rows == number_of_data_points + 1
+        number_of_splits = number_of_data_points // self.row_limit
+        if number_of_data_points % self.row_limit:
+            number_of_splits += 1
+        number_of_rows = sum([self._write_to_csv(output_file.split('.')[0] +
+                              '_' + str(index) + '.csv', index)
+                              for index in xrange(number_of_splits)])
+        return number_of_rows == number_of_data_points + number_of_splits
 	        
     def _write_to_csv(self, output_file, index=0):
     	"""
     	Helper method to write rows to csv files
     	"""
-    	output_file_path = os.path.abspath(os.path.join(self.output_directory, output_file))
+    	output_file_path = os.path.abspath(os.path.join(self.output_directory,
+                                           output_file))
         with open(output_file_path, 'w') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(self.headers)
+            row_count = 1
             for row in (item for item in self.csv_data[index * self.row_limit :
                        (index + 1) * self.row_limit]):
-                # This loop looks for unicode objects and encodes them to ASCII to avoif Unicode errors,
-                # for e.g. UnicodeEncodeError: 'ascii' codec can't encode character u'\xf1'
+                # This loop looks for unicode objects and encodes them to 
+                # ASCII to avoid Unicode errors, for e.g. UnicodeEncodeError: 
+                # 'ascii' codec can't encode character u'\xf1'
                 row = [item.encode('ascii', 'ignore')  if isinstance(item, unicode)
                        else item for item in row]
                 writer.writerow(row)
-		
+		row_count += 1
+        return row_count
+
     @property
     def collections(self):
     	return self._collections
     	
     @collections.setter
     def collections(self, _collections):
-    	self._collections = {collection : self.db[collection] for collection in _collections}
+    	self._collections = {collection : self.db[collection]
+                             for collection in _collections}
 
     def report_name(self, *args):
         return '-'.join(item for item in args) + '.csv'
