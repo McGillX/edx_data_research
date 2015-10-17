e23329946f5e51b1b18879ffd232bd1311f59342
ueg1990 <uehtesham90@gmail.com>
2015-10-17 15:08:59 -0400

    Define methods to get gender stats, age stats and certificate stats

--- edx_data_research/reporting/edx_stats.py
+++ edx_data_research/reporting/edx_stats.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict
 from datetime import date
 
 from edx_data_research.reporting.edx_base import EdX
@@ -7,13 +7,68 @@ class Stats(EdX):
 
     def __init__(self, args):
         super(self.__class__, self).__init__(args)
+        self.number_of_students = 0
 
     def stats(self):
         """Return general stats for a given course """
-        self.collections = ['auth_userprofile']
-        self._age()
+        self.collections = ['auth_userprofile', 'certificates_generatedcertificate']
+        self.number_of_students = self.collections['auth_userprofile'].count()
+        age_stats = self._age()
+        gender_stats = self._gender()
+        certificate_stats = self._certificate()
+	print certificate_stats
 
     def _age(self):
+        age_breakdown = defaultdict(int)
         current_year = date.today().year
-        print current_year
-        
+        cursor = self.collections['auth_userprofile'].find()
+	for item in cursor:
+	    year_of_birth = item['year_of_birth']
+            if year_of_birth != 'NULL':
+                age = current_year - int(year_of_birth)
+		if age < 20:
+		    age_breakdown['Age - Under 20'] += 1
+		elif 20 <= age <= 29:
+		    age_breakdown['Age - 20-29'] += 1
+ 		elif 30 <= age <= 39:
+		    age_breakdown['Age - 30-39'] += 1
+		elif 40 <= age <= 49:
+		    age_breakdown['Age - 40-49'] += 1
+		elif 50 <= age <= 69:
+		    age_breakdown['Age - 50-69'] += 1
+		elif age >= 70:
+		    age_breakdown['Age - 70+'] += 1
+	    else:
+		age_breakdown['Age - None'] += 1
+        return age_breakdown
+
+    def _gender(self):
+        gender_breakdown = defaultdict(int)
+        cursor = self.collections['auth_userprofile'].find()
+ 	for item in cursor:
+	    gender = item['gender']
+	    if gender == 'm':
+                gender_breakdown['Gender - Male'] += 1
+	    elif gender == 'f':
+                gender_breakdown['Gender - Female'] += 1
+	    elif gender == 'o':
+                gender_breakdown['Gender - Other'] += 1
+            else:
+                gender_breakdown['Gender - None'] += 1
+        gender_breakdown = {key: value * 100.0 / self.number_of_students
+                            for key, value in gender_breakdown.iteritems()}
+        return gender_breakdown
+
+    def _certificate(self):
+        certificate_breakdown = defaultdict(int)
+        cursor = self.collections['certificates_generatedcertificate'].find()
+        for item in cursor:
+            status = item['status']
+            if status == 'notpassing':
+                certificate_breakdown['Certificate - No'] += 1
+            elif status == 'downloadable':
+                certificate_breakdown['Certificate - Yes'] += 1
+        certificate_breakdown = {key: value * 100.0 / self.number_of_students
+                            for key, value in certificate_breakdown.iteritems()}
+        return certificate_breakdown 
+
