00bde0f28b99d8263f384ee593a8eed9eb300e3e
ueg1990 <uehtesham90@gmail.com>
2015-10-26 00:12:51 -0400

    Define new base class

---
+++ edx_data_research/edx_base.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+
+class EdX(object):
+
+    def __init__(self, args):
+        self.uri = args.uri
+        self.db_name = args.db_name
+        self.db = None
+        self._collections = None
+
+    @property
+    def collections(self):
+    	return self._collections
+    	
+    @collections.setter
+    def collections(self, _collections):
+    	self._collections = {collection : self.db[collection]
+                             for collection in _collections}
+
