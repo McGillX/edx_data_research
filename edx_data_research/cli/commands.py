2dd342603051b2f311fc7a428d4dc4e3b79cb376
ueg1990 <uehtesham90@gmail.com>
2015-10-28 00:11:02 -0400

    Define proxy function for generating problem ids collection

--- edx_data_research/cli/commands.py
+++ edx_data_research/cli/commands.py
@@ -3,6 +3,7 @@ In this module we define the interface between the cli input provided
 by the user and the analytics required by the user
 """
 from edx_data_research.parsing.forum.parse_forum import Forum
+from edx_data_research.parsing.problem_ids import ProblemIdsCollection
 from edx_data_research.parsing.sql.parse_sql import SQL
 from edx_data_research.reporting.edx_basic import Basic
 from edx_data_research.reporting.edx_problem_ids import ProblemIds
@@ -36,4 +37,8 @@ def cmd_parse_sql(args):
 
 def cmd_parse_forum(args):
     edx_obj = Forum(args)
+    edx_obj.migrate()
+
+def cmd_parse_problem_ids(args):
+    edx_obj = ProblemIdsCollection(args)
     edx_obj.migrate()
