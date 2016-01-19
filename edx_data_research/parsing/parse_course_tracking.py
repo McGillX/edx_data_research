19758a3067f0cfaa5c7059d93ae01c7f99d51394
ueg1990 <uehtesham90@gmail.com>
2016-01-18 23:30:42 -0500

    Fix indendation issues

--- edx_data_research/parsing/parse_course_tracking.py
+++ edx_data_research/parsing/parse_course_tracking.py
@@ -22,34 +22,34 @@ class CourseTracking(Parse):
         self.tracking_tracking = self.client['tracking']['tracking']
 
     def migrate(self):
-    	course_ids, start_date, end_date = self._load_config_file(
-    									       self.course_config_file)
-    	self._extract_tracking_logs(course_ids, start_date, end_date)
+        course_ids, start_date, end_date = self._load_config_file(
+                                               self.course_config_file)
+        self._extract_tracking_logs(course_ids, start_date, end_date)
 
     def _load_config_file(self, course_config_file):
-    	'''
-	    Return course ids and ranges of dates from which course specific
-	    tracking logs will be extracted
-	    '''
-	    with open(course_config_file) as file_handler:
-	        data = json.load(file_handler)
+        '''
+        Return course ids and ranges of dates from which course specific
+        tracking logs will be extracted
+        '''
+        with open(course_config_file) as file_handler:
+            data = json.load(file_handler)
         if not isinstance(data['course_ids'], list):
             raise TypeError('Expecting list of course ids')
         try:
             start_date = datetime.strptime(data['date_of_course_enrollment'],
-            	                           '%Y-%m-%d')
+                                           '%Y-%m-%d')
             end_date = datetime.strptime(data['date_of_course_completion'],
-            	                         '%Y-%m-%d')
+                                         '%Y-%m-%d')
         except ValueError:
             raise ValueError('Incorrect data format, should be YYYY-MM-DD')
-	    return data['course_ids'], start_date.date(), end_date.date()
+        return data['course_ids'], start_date.date(), end_date.date()
 
-	def _append_course_structure_data(course_structure_collection, _id, document):
-		'''
-	    Append parent_data and metadata (if exists) from course structure to 
-	    tracking log
-	    '''
-	    output = {}
+    def _append_course_structure_data(course_structure_collection, _id, document):
+        '''
+        Append parent_data and metadata (if exists) from course structure to 
+        tracking log
+        '''
+        output = {}
         data = course_structure_collection.find({"_id" : _id})[0]
         if 'parent_data' in data:
             output['parent_data'] = data['parent_data']
@@ -57,40 +57,40 @@ class CourseTracking(Parse):
             output['metadata'] = data['metadata']
         return output
 
-	def _extract_tracking_logs(self, course_ids, start_date, end_date):
-		'''
-	    Return all trackings logs that contain given ids and that contain dates
-	    within the given range
-	    '''
-	    documents = self.tracking_tracking.find({'course_id' : {'$in' :
-	    	                                                    course_ids}})
-	    for document in documents:
-	        if (start_date <= datetime.strptime(document['time']
-	        	              .split('T')[0], "%Y-%m-%d").date() <= end_date):
-	            # Bind parent_data and metadata from course_structure to
-	            # tracking document
-	            bound = False
-	            if document['event']:
-	                if isinstance(document['event'], dict):
-	                    if 'id' in document['event']:
-	                        splitted = document['event']['id'].split('-')
-	                        if len(splitted) > 3:
-	                            document['event']['id'] = splitted[-1]
-	                            if not bound:
-	                            	document.update(
-	                            		self._append_course_structure_data(
-	                                	self.collections['course_structure'],
-	                                	document['event']['id']))
-	                                bound = True
-	            if document['page']:
-	                splitted = document['page'].split('/')
-	                if len(splitted) > 2:
-	                    document['page'] = splitted[-2]
-	                    if not bound:
-	                    	document.update(
-                        		self._append_course_structure_data(
-                            	self.collections['course_structure'],
-                            	document['page']))
-	            # End of binding, now insert document into collection
-	            self.collections['tracking'].insert(document)
+    def _extract_tracking_logs(self, course_ids, start_date, end_date):
+        '''
+        Return all trackings logs that contain given ids and that contain dates
+        within the given range
+        '''
+        documents = self.tracking_tracking.find({'course_id' : {'$in' :
+                                                                course_ids}})
+        for document in documents:
+            if (start_date <= datetime.strptime(document['time']
+                              .split('T')[0], "%Y-%m-%d").date() <= end_date):
+                # Bind parent_data and metadata from course_structure to
+                # tracking document
+                bound = False
+                if document['event']:
+                    if isinstance(document['event'], dict):
+                        if 'id' in document['event']:
+                            splitted = document['event']['id'].split('-')
+                            if len(splitted) > 3:
+                                document['event']['id'] = splitted[-1]
+                                if not bound:
+                                    document.update(
+                                        self._append_course_structure_data(
+                                        self.collections['course_structure'],
+                                        document['event']['id']))
+                                    bound = True
+                if document['page']:
+                    splitted = document['page'].split('/')
+                    if len(splitted) > 2:
+                        document['page'] = splitted[-2]
+                        if not bound:
+                            document.update(
+                                self._append_course_structure_data(
+                                self.collections['course_structure'],
+                                document['page']))
+                # End of binding, now insert document into collection
+                self.collections['tracking'].insert(document)
 
