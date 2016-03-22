eff1cc5f1403edd464d58fdf5f98bde8e6c8b8d1
ueg1990 <uehtesham90@gmail.com>
2016-03-21 22:17:35 -0400

    Remove confirmable feature flag

--- edx_data_research/web_app/auth/forms.py
+++ edx_data_research/web_app/auth/forms.py
@@ -2,5 +2,6 @@ from flask_security.forms import RegisterForm
 from wtforms import StringField
 from wtforms.validators import Required
 
+
 class ExtendedRegisterForm(RegisterForm):
     username = StringField('Username', [Required(message='Username not provided')])
