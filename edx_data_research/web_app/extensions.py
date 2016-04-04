464e09e1d738007a7abc48791776a40d5bb0059c
ueg1990 <uehtesham90@gmail.com>
2016-04-03 20:48:54 -0400

    Integrate flask-boostrap

--- edx_data_research/web_app/extensions.py
+++ edx_data_research/web_app/extensions.py
@@ -1,8 +1,10 @@
+from flask_bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.security import Security
 
 
+bootstrap = Bootstrap()
 db = MongoEngine()
 mail = Mail()
 security = Security()
