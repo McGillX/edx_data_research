8d01c9039a341ab58ed8b69322bd3d63e232f8cb
ueg1990 <uehtesham90@gmail.com>
2016-04-14 00:05:46 -0400

    Define index view of parse

--- edx_data_research/web_app/parse/views.py
+++ edx_data_research/web_app/parse/views.py
@@ -1,7 +1,10 @@
 from flask import render_template
+from flask.ext.security import login_required
 
 from . import parse
 
+
 @parse.route('/')
+@login_required
 def index():
-    pass
+    return render_template('parse/index.html')
