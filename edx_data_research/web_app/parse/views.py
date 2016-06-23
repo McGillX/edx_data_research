1d8a8753c2d549229255d09fa8e67b11c2b37ba0
ueg1990 <uehtesham90@gmail.com>
2016-06-23 02:47:49 -0400

    Add starting part for parsing module

--- edx_data_research/web_app/parse/views.py
+++ edx_data_research/web_app/parse/views.py
@@ -8,3 +8,32 @@ from . import parse
 @login_required
 def index():
     return render_template('parse/index.html')
+
+
+@parse.route('/sql', methods=['GET', 'POST'])
+@login_required
+def parse_sql():
+    return render_template('parse/index.html')
+
+@parse.route('/forum', methods=['GET', 'POST'])
+@login_required
+def parse_forum():
+    return render_template('parse/index.html')
+
+
+@parse.route('/coursestructure', methods=['GET', 'POST'])
+@login_required
+def parse_course_structure():
+    return render_template('parse/index.html')
+
+
+@parse.route('/problemids', methods=['GET', 'POST'])
+@login_required
+def parse_problem_ids():
+    return render_template('parse/index.html')
+
+
+@parse.route('/coursetracking', methods=['GET', 'POST'])
+@login_required
+def parse_course_tracking():
+    return render_template('parse/index.html')
